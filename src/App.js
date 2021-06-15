65f54e3c4af41b16de8c3ba52f56f9bf4beda473
Gayashan Wagachchi <gayashan4lk@gmail.com>
2021-06-15 18:16:51 +0530

    added file : navbar.jsx

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Cards from "./components/cards";
 import TagList from "./components/taglist";
 import Counters from "./components/counters";
+import NavBar from "./components/navbar";
 
 function App() {
   return (
       <div>
+          <NavBar/>
           <Counters/>
           <Cards/>
           <TagList/>
