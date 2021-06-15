b0fdc849b3bf2837cc420d41f0fa0910c0c49bcc
Gayashan Wagachchi <gayashan4lk@gmail.com>
2021-06-15 17:39:33 +0530

    add new counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,44 @@ import React, {Component} from "react";
 
 class Counter extends Component {
     state = {
-    }
+        value: this.props.counter.value
+    };
 
     render() {
         return (
             <div className='counter-container'>
-                <span className="badge badge-primary m-2">zero</span>
-                <button className="btn btn-sm btn-warning m-2">Increment</button>
+                <span className={this.getBadgeClasses()}><h4>{this.formatCount()}</h4></span>
+                <button onClick={this.handleIncrement} className="btn btn-sm btn-success m-1">
+                    <i className="fa fa-plus fa-lg" aria-hidden="true"> </i>
+                </button>
+                <button onClick={this.handleDecrement} className="btn btn-sm btn-secondary m-1">
+                    <i className="fa fa-minus fa-lg" aria-hidden="true"> </i>
+                </button>
+                <button onClick={() => {this.props.onDelete(this.props.counter.id)}} className="btn btn-sm btn-danger m-1">
+                    <i className="fa fa-times fa-lg" aria-hidden="true"> </i>
+                </button>
             </div>
         );
     }
+
+    handleIncrement = () => {
+        this.setState({value:this.state.value + 1});
+    };
+
+    handleDecrement = () => {
+        this.setState({value:this.state.value - 1});
+    }
+
+    formatCount() {
+        return this.state.value === 0 ? "Zero" : this.state.value;
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-3 badge-";
+        classes += this.state.value === 0 ? 'warning' : 'primary';
+        return classes;
+    }
+
 }
 
 export default Counter;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,15 +2,46 @@ import React, {Component} from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-    state = {}
+    state = {
+        counters: [
+            {id: 1, value: 0},
+            {id: 2, value: 1},
+            {id: 3, value: 3},
+            {id: 4, value: 5}
+        ]
+    };
 
     render() {
         return (
             <div className='counters-main-container'>
-                <Counter/>
+                <h5><b>Single source of truth</b></h5>
+                <hr/>
+                <button
+                    onClick={this.handleReset}
+                    className="btn btn-primary btn-sm m-2"><h5><b>Reset</b></h5></button>
+                {this.state.counters.map(counter => (
+                    <Counter
+                        key={counter.id}
+                        onDelete={this.handleDelete}
+                        counter={counter}
+                    />
+                ))}
             </div>
         )
     }
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({counters: counters})
+    };
+
+    handleDelete = (counterID) => {
+        const counters = this.state.counters.filter(c => c.id !== counterID);
+        this.setState({counters: counters});
+    };
 }
 
 export default Counters;
